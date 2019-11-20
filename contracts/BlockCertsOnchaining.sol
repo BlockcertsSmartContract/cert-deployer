ac679ca70e2b06678f1e386d228c2dc700b84af5
marc <m.radau@campus.tu-berlin.de>
2019-11-20 16:26:44 +0100

    BlockCertsOnchaining.sol: only owner can issue certificates - issuer.py: catch exception (distinguishing exception type?)

--- contracts/BlockCertsOnchaining.sol
+++ contracts/BlockCertsOnchaining.sol
@@ -2,7 +2,17 @@ pragma solidity >0.5.0;
 
 contract BlockCertsOnchaining {
     uint256 public certCount = 0;
-    uint public testVal = 13;
+
+    address internal owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
     
     mapping(uint => Certificate) public certs;
     
@@ -11,7 +21,7 @@ contract BlockCertsOnchaining {
         address _issuer;
     }
     
-    function issueCert(uint256 _merkleRootHash) public {
+    function issueCert(uint256 _merkleRootHash) public onlyOwner {
         // check if msg.sender is allowed to issue
         certs[certCount] = Certificate(_merkleRootHash, msg.sender);
         certCount += 1;
--- python_tools/issuer.py
+++ python_tools/issuer.py
@@ -2,13 +2,30 @@
 
 from connections import ContractConnection
 
+# always accesses last deployed contract instance
 c = ContractConnection('http://localhost:8545','../build/contracts/BlockCertsOnchaining.json')
 issuer = c.create_contract_object()
 
 # print("available functions: ", issuer.all_functions())
+print("# of certs issued so far")
+print(issuer.functions.certCount().call())
+
+print("issuing cert with hash 666")
+issuer.functions.issueCert(666).transact()
+
+print("# of certs issued so far")
+print(issuer.functions.certCount().call())
 
 c.set_w3_wallet(1)
 
-issuer.functions.issueCert(666).transact()
+print("trying to issue cert with hash 666")
+try:
+    issuer.functions.issueCert(666).transact()
+except:
+    print("only owner")
+
+print("# of certs issued so far")
 print(issuer.functions.certCount().call())
-print(issuer.functions.testVal().call())
+
+# print("test value (hardcoded)")
+# print(issuer.functions.testVal().call())
