97b18f43183777be985e2c8b3ac6789ce72bdad2
marc <m.radau@campus.tu-berlin.de>
2019-11-21 23:15:19 +0100

    python_tools model issuing, revocation, verification

--- python_tools/cert.py
+++ python_tools/cert.py
@@ -1,6 +1,47 @@
 class Certificate:
-    def __init__(self, merkleRootHash, certHash):
+    def __init__(self, merkleRootHash, certHash, contract_obj):
         self.merkleRootHash = merkleRootHash
         self.certHash = certHash
+        self.contract_obj = contract_obj
+
+    def issueBatch(self):
+        try:
+            self.contract_obj.functions.issueBatch(self.merkleRootHash).transact()
+        except ValueError:
+            print("could not issue batch with merkleRootHash " + str(self.merkleRootHash) + ". No permission?")
+
+    def revokeBatch(self):
+        try:
+            self.contract_obj.functions.revokeBatch(self.merkleRootHash).transact()
+        except ValueError:
+            print("could not revoke batch with batch hash " + str(self.merkleRootHash) + ". No permission?")
+
+    def revokeCert(self):
+        try:
+            self.contract_obj.functions.revokeCert(self.certHash).transact()
+        except ValueError:
+            print("could not revoke cert with cert hash " + str(self.certHash) + ". No permission?")
+
+    def getCertInfo(self):
+        try:
+            return self.contract_obj.functions.revokedCerts(self.certHash).call()
+        except:
+            print("Could not get certificate by certHash: " + str(self.certHash) + ". Correct data type?")
+
+    def getBatchInfo(self):
+        try:
+            return self.contract_obj.functions.batches(self.merkleRootHash).call()
+        except:
+            print("Could not get batch by batchHash: " + str(self.merkleRootHash) + ". Correct data type?")
+
+    def isCertValid(self, verbose=True):
+        batchInfo = self.getBatchInfo()
+        certInfo = self.getCertInfo()
+        valid = not (batchInfo[0] == 0 or batchInfo[1] == True or certInfo == True)
+        
+        if verbose:
+            print("batch with batch merkleRootHash: " + str(batchInfo[0]) + " is revoked: " + str(batchInfo[1]))
+            print("cert with certHash " + str(self.certHash) + " from batch " + str(batchInfo[0]) + " is revoked: " + str(certInfo))
+            print("cert is valid: " + str(valid))
 
 
--- python_tools/issuer.py
+++ python_tools/issuer.py
@@ -3,57 +3,19 @@
 from connections import ContractConnection
 from cert import Certificate
 
-
 # always accesses last deployed contract instance
-c = ContractConnection('http://localhost:8545','../build/contracts/BlockCertsOnchaining.json')
-issuer = c.get_contract_object()
+contract_conn = ContractConnection('http://localhost:8545','../build/contracts/BlockCertsOnchaining.json')
+contract_obj = contract_conn.get_contract_object()
 
-# wrap function calls
-def issueBatch(merkleRootHash):
-    try:
-        issuer.functions.issueBatch(merkleRootHash).transact()
-    except ValueError:
-        print("could not issue batch with merkleRootHash " + str(merkleRootHash) + ". No permission?")
+c1 = Certificate(10, 11, contract_obj)
 
-def revokeBatch(batchHash):
-    try:
-        issuer.functions.revokeBatch(batchHash).transact()
-    except ValueError:
-        print("could not revoke batch with batch hash " + str(batchHash) + ". No permission?")
-
-def revokeCert(certHash):
-    try:
-        issuer.functions.revokeCert(certHash).transact()
-    except ValueError:
-        print("could not revoke cert with cert hash " + str(certHash) + ". No permission?")
-
-def getCertInfo(certHash):
-    try:
-        return issuer.functions.revokedCerts(certHash).call()
-    except:
-        print("Could not get certificate by certHash: " + str(certHash) + ". Correct data type?")
-
-def getBatchInfo(batchHash):
-    try:
-        return issuer.functions.batches(batchHash).call()
-    except:
-        print("Could not get batchificate by batchHash: " + str(batchHash) + ". Correct data type?")
-
-cert1 = Certificate(5,3)
-
-def isCertValid(cert):
-    batch_info = getBatchInfo(cert.merkleRootHash)
-    cert_info = getCertInfo(cert.certHash)
-    if batch_info[0] == 0:
-        return False
-    if batch_info[1] == True:
-        return False
-    if cert_info == True:
-        return False
-    return True
-
-issueBatch(cert1.merkleRootHash)
-print("is cert1 valid? " + str(isCertValid(cert1)))
-revokeCert(cert1.certHash)
-# revokeBatch(cert1.merkleRootHash)
-print("is cert1 valid? " + str(isCertValid(cert1)))
+print("issue batch:")
+c1.issueBatch()
+c1.isCertValid()
+print()
+print("revoke Batch")
+c1.revokeBatch()
+c1.isCertValid()
+print()
+c1.revokeCert()
+c1.isCertValid()
