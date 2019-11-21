f5bffb7fdd7ccfc38751827e7bb9cdb6fccdbe0d
marc <m.radau@campus.tu-berlin.de>
2019-11-21 19:37:47 +0100

    python_tools validity check

---
+++ python_tools/cert.py
@@ -0,0 +1,6 @@
+class Certificate:
+    def __init__(self, merkleRootHash, certHash):
+        self.merkleRootHash = merkleRootHash
+        self.certHash = certHash
+
+
--- python_tools/issuer.py
+++ python_tools/issuer.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python
 
 from connections import ContractConnection
+from cert import Certificate
+
 
 # always accesses last deployed contract instance
 c = ContractConnection('http://localhost:8545','../build/contracts/BlockCertsOnchaining.json')
@@ -37,11 +39,21 @@ def getBatchInfo(batchHash):
     except:
         print("Could not get batchificate by batchHash: " + str(batchHash) + ". Correct data type?")
 
-issueBatch(12)
-print(getBatchInfo(12))
-print(getBatchInfo(11))
-revokeBatch(12)
-print(getBatchInfo(12))
-revokeBatch(12)
-revokeCert(13)
-print(getCertInfo(13))
+cert1 = Certificate(5,3)
+
+def isCertValid(cert):
+    batch_info = getBatchInfo(cert.merkleRootHash)
+    cert_info = getCertInfo(cert.certHash)
+    if batch_info[0] == 0:
+        return False
+    if batch_info[1] == True:
+        return False
+    if cert_info == True:
+        return False
+    return True
+
+issueBatch(cert1.merkleRootHash)
+print("is cert1 valid? " + str(isCertValid(cert1)))
+revokeCert(cert1.certHash)
+# revokeBatch(cert1.merkleRootHash)
+print("is cert1 valid? " + str(isCertValid(cert1)))
