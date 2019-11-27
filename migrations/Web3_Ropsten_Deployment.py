ff92c18f07c6c0d8d0ef3688507f64ad4594ba90
flo-weiss <flw@wip.tu-berlin.de>
2019-11-27 16:28:13 +0100

    code now contains flags for ganache and ropsten

--- migrations/Web3_Ropsten_Deployment.py
+++ migrations/Web3_Ropsten_Deployment.py
@@ -1,13 +1,7 @@
 import json
 from Web3 import web3 
 
-#if using ganache
-#ganache_url = "" #insert ganache url
-#web3 = Web3(Web3.HTTPProvider(ganache_url))
-
-#if using ropsten infura node 
-Web3 = require('web3')
-web3 = Web3('https://ropsten.infura.io/hqRzEqFKv6IsjRxfVUWH')
+ganache = false #FLAG --> use of ropsten
 
 issuer = ''#insert address with 0x prefix here
 
@@ -16,7 +10,15 @@ data = ''#insert contract ABI here from remix
 
 contract = new web3.etc.Contract(data, smartcontractaddress)
 
-privateKeyIssuer = ""#insert key here
+if(ganache):
+	ganache_url = "" #INSERT ganache url
+	web3 = Web3(Web3.HTTPProvider(ganache_url))
+
+else:#if using ropsten infura node 
+	Web3 = require('web3')
+	web3 = Web3('https://ropsten.infura.io/hqRzEqFKv6IsjRxfVUWH')
+
+	privateKeyIssuer = ""#insert key here	
 
 nonce = web3.eth.getTransactionCount(issuer)
 
@@ -28,8 +30,16 @@ txObject = {
 	data: contract.methods.X(argument).encodeABI() #call function X with respective parameters. look up available (external) functions in remix too
 }
 
-tx = web3.eth.account.signTransaction(txObject, privateKeyIssuer)
+
+if (ganache == false): #only need to sign if we want to access to ropsten with locked account
+	tx = web3.eth.account.signTransaction(txObject, privateKeyIssuer)
+
 tx_hash = web3.eth.sendRawTransaction(tx.rawTransaction)
 
 print(web3.toHex(tx_hash))
+
+#USUEFUL:
+	#web3.personal.lockAccount('0xE0ca...c1f7')
+	#web3.personal.unlockAccount('0xE0ca...c1f7', 'mypass')
+
 
