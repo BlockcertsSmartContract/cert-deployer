88f92173c957a701958a08c7443f3bc9005f6cb4
flo-weiss <flw@wip.tu-berlin.de>
2019-11-27 19:29:22 +0100

    having transcations for web3 ready

--- migrations/Web3_Ropsten_Deployment.py
+++ migrations/Web3_Ropsten_Deployment.py
@@ -4,20 +4,22 @@ from Web3 import web3
 ganache = false #FLAG --> use of ropsten
 
 issuer = ''#INSERT address with 0x prefix here
+privateKeyIssuer = "" #INSERT
 
 smartcontractaddress = ''#insert address with 0x prefix here
 data = json.loads('')#insert contract ABI here from remix
 
 contract = new web3.etc.Contract(data, smartcontractaddress)
 
+nodeurl = 'https://ropsten.infura.io/hqRzEqFKv6IsjRxfVUWH'
+
 if(ganache):
-	ganache_url = "" #INSERT
-	web3 = Web3(Web3.HTTPProvider(ganache_url))
+	nodeurl = 'http://localhost:8545' #INSERT
 
-else:
-	web3 = Web3('https://ropsten.infura.io/hqRzEqFKv6IsjRxfVUWH')
-	privateKeyIssuer = "" #INSERT
-
+elif(!ganache):
+	privateKeyIssuer = "" #INSERT, we only need this key if locked non-ganache account
+	
+web3 = Web3(nodeurl)
 nonce = web3.eth.getTransactionCount(issuer)
 
 txObject = {
