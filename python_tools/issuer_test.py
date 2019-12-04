1200c7f1802d10e73a46904c5af065614824b628
Ahmet Flamestro <19ahmet99@gmail.com>
2019-12-05 00:00:52 +0100

    repaired tests

--- python_tools/issuer_test.py
+++ python_tools/issuer_test.py
@@ -1,32 +1,27 @@
 import unittest
 
-import onchaining_tools.path_tools as tools
-from onchaining_tools.cert import Certificate
-from onchaining_tools.connections import TruffleContract
+from onchaining_tools.connections import ContractConnection
 
-contract_conn = TruffleContract(tools.get_host(), tools.get_contract_as_json_path())
-contract_obj = contract_conn.get_contract_object()
+sc = ContractConnection()
 
 
 class issuerTest(unittest.TestCase):
 
     def test_whenIssueCert_thenCertIsValidIsTrue(self):
-        c1 = Certificate(100, 50, contract_obj)
-        c1.issue()
-        self.assertTrue(c1.is_cert_valid())
+        cert_hash = 1000000
+
+        sc.functions.issue(cert_hash)
+
+        self.assertTrue(not sc.functions.get_status(cert_hash))
 
     def test_whenRevokeCert_thenCertIsValidIsFalse(self):
-        c2 = Certificate(70, 90, contract_obj)
-        c2.issue()
-        c2.revoke_cert()
-        self.assertFalse(c2.is_cert_valid())
+        cert_hash = 1000001
 
-    def test_whenRevokeBatch_thenCertIsValidIsFalse(self):
-        c3 = Certificate(705, 905, contract_obj)
-        c3.issue()
-        c3.revoke_batch()
-        self.assertFalse(c3.is_cert_valid())
+        sc.functions.issue(cert_hash)
+        self.assertTrue(not sc.functions.get_status(cert_hash))
 
+        sc.functions.revoke(cert_hash)
+        self.assertTrue(sc.functions.get_status(cert_hash))
 
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
