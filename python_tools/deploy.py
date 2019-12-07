8021ac75057ae30627bbc8a3bce6a2aeaa2d4a5a
Xenia Melman <x.melman@campus.tu-berlin.de>
2019-12-07 16:19:58 +0100

    comments

--- python_tools/deploy.py
+++ python_tools/deploy.py
@@ -9,59 +9,73 @@ from solc import compile_standard
 
 
 class ContractDeployer(object):
+    ''' Compiles, signes and deploys a smart contract on the ethereum blockchain
+    Args:
+        object(web3 object): instantiated web3 connection to ethereum node
+    '''
     def __init__(self):
+        '''Defines blockchain, initializes ethereum wallet, calls out compilation and deployment functions'''
         current_chain = config.config["current_chain"]
         self.pubkey = config.config["wallets"][current_chain]["pubkey"]
-
         w3Factory = MakeW3()
         self.w3 = w3Factory.get_w3_obj()
         self.acct = w3Factory.get_w3_wallet()
         self.compile_contract()
         self.deploy()
+        #
         if current_chain == "ropsten":
             self.assign_ens()
 
     def compile_contract(self):
+        '''Compiles smart contract, creates bytecode and abi'''
+
+        #loading contract file data
         with open(tools.get_contract_path()) as source_file:
             source_raw = source_file.read()
-
+        #loading configuration data
         with open(tools.get_compile_data_path()) as opt_file:
             raw_opt = opt_file.read()
             opt = json.loads(raw_opt)
 
         opt["sources"]["BlockCertsOnchaining.sol"]["content"] = source_raw
         compiled_sol = compile_standard(opt)
+
+        #defining bytecode and abi
         self.bytecode = compiled_sol[
             'contracts']['BlockCertsOnchaining.sol']['BlockCertsOnchaining']['evm']['bytecode']['object']
         self.abi = json.loads(compiled_sol[
             'contracts']['BlockCertsOnchaining.sol']['BlockCertsOnchaining']['metadata'])['output']['abi']
 
     def deploy(self):
+        '''Signes raw transaction and deploys it on the blockchain'''
         contract = self.w3.eth.contract(abi=self.abi, bytecode=self.bytecode)
 
+        #defining blockchain and public key of the ethereum wallet
         current_chain = config.config["current_chain"]
         acct_addr = config.config["wallets"][current_chain]["pubkey"]
 
+        #building raw transaction
         construct_txn = contract.constructor().buildTransaction({
             'nonce': self.w3.eth.getTransactionCount(acct_addr),
             'gas': 1000000
         })
 
+        #signing & sending a signed transaction, saving transaction hash
         signed = self.acct.sign_transaction(construct_txn)
         tx_hash = self.w3.eth.sendRawTransaction(signed.rawTransaction)
         tx_receipt = self.w3.eth.waitForTransactionReceipt(tx_hash)
 
+        #saving contract data
         with open(tools.get_contr_info_path(), "r") as f:
             raw = f.read()
             contr_info = json.loads(raw)
-
         self.contr_address = tx_receipt.contractAddress
         data = {'abi': self.abi, 'address': self.contr_address}
         contr_info["blockcertsonchaining"] = data
-
         with open(tools.get_contr_info_path(), "w+") as f:
             json.dump(contr_info, f)
 
+        #print transaction hash
         print(f"deployed contr <{self.contr_address}>")
 
     def assign_ens(self):
@@ -85,6 +99,9 @@ class ContractDeployer(object):
 
 
 if __name__ == '__main__':
+    '''Parses arguments and calls out respective functionatilites.
+        Args: [ropsten/ganache] 
+    '''
     parser = argparse.ArgumentParser()
     # args: deploy local or remote
     parser.add_argument("provider", help="supported providers are ropsten and ganache", type=str)
--- python_tools/onchaining_tools/connections.py
+++ python_tools/onchaining_tools/connections.py
@@ -6,7 +6,11 @@ from web3 import Web3, HTTPProvider
 
 
 class MakeW3(object):
+    '''Defines a private key of an ethereum wallet to be used for the transaction, 
+        node url to be used for communication with ethereum blockchain and instantiates the
+        web3 connection with ethereum node '''
     def __init__(self):
+        '''Defines public & private keys of a wallet, defines an ethereum node, that will be used for communication with blockchain'''
         self.privkey = config.config["wallets"][config.config["current_chain"]]["privkey"]
         self.pubkey = config.config["wallets"][config.config["current_chain"]]["pubkey"]
         self.url = config.config["wallets"][config.config["current_chain"]]["url"]
@@ -14,17 +18,20 @@ class MakeW3(object):
         self.w3.eth.defaultAccount = self.pubkey
 
     def create_w3_obj(self):
+        '''Instantiates a web3 connection with ethereum node'''
         return Web3(HTTPProvider(self.url))
 
     def get_w3_obj(self):
         return self.w3
 
     def get_w3_wallet(self):
+        '''Connects a private key to the account that is going to be used for the transaction'''
         return self.w3.eth.account.from_key(self.privkey)
 
 
 class ContractConnection(object):
-    def __init__(self, contract_name="ropsten"):  # "blockcertsonchaining", "ropsten_ens_registry", "ropsten_ens_resolver"
+    '''Collects abi, address, contract data and instantiates a contract object'''
+        def __init__(self, contract_name="ropsten"):  # "blockcertsonchaining", "ropsten_ens_registry", "ropsten_ens_resolver"
         self.contract_name = contract_name
         self.w3 = MakeW3().get_w3_obj()
         self.contract_info = self.get_contract_info()
@@ -32,23 +39,28 @@ class ContractConnection(object):
         self.functions = ContractFunctions(self.w3, self.contract_obj)
 
     def create_contract_object(self):
+        '''Returns contract address and abi'''
         address = self.get_address()
         abi = self.get_abi()
         return self.w3.eth.contract(address=address, abi=abi)
 
     def get_contract_object(self):
+        '''Returns instantiated contract'''
         return self.contract_obj
 
     def get_contract_info(self):
+        '''Returns transaction data from a config file'''
         with open(tools.get_contr_info_path()) as file:
             data = file.read()
             contract_info = json.loads(data)
         return contract_info
 
     def get_abi(self):
+        '''Returns transaction abi'''
         return self.contract_info[self.contract_name]["abi"]
 
     def get_address(self):
+        '''Returns transaction address'''
         return self.contract_info[self.contract_name]["address"]
 
 
@@ -62,6 +74,7 @@ class ContractFunctions(object):
         self.acct_addr = config.config["wallets"][current_chain]["pubkey"]
 
     def get_tx_options(self):
+        '''Returns raw transaction'''
         return {
             'nonce': self.w3.eth.getTransactionCount(self.acct_addr),
             'gas': 100000
@@ -69,6 +82,7 @@ class ContractFunctions(object):
         }
 
     def transact(self, method, *argv):
+        ''''''
         acct = self.w3.eth.account.from_key(self.privkey)
         tx_options = self.get_tx_options()
         construct_txn = self.contract_obj.functions[method](*argv).buildTransaction(tx_options)
