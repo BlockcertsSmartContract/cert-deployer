8ec5d05a752a4f9a001d256cf8087d4642f367ab
Ahmet Flamestro <19ahmet99@gmail.com>
2019-12-14 17:20:52 +0100

    add ipfs content hash to ens

--- python_tools/deploy.py
+++ python_tools/deploy.py
@@ -13,9 +13,17 @@ class ContractDeployer(object):
     ''' Compiles, signes and deploys a smart contract on the ethereum blockchain
     Args:
         object(web3 object): instantiated web3 connection to ethereum node
+            print("cat : " + str(client.cat(res['Hash'])))
     '''
+
     def __init__(self):
         '''Defines blockchain, initializes ethereum wallet, calls out compilation and deployment functions'''
+        try:
+            self._client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001/http')
+            print("connected to IPFS")
+        except:
+            print("Not connected to IPFS -> start daemon to deploy contract info on IPFS")
+            self._client = None
         current_chain = config.config["current_chain"]
         w3Factory = MakeW3()
         self.w3 = w3Factory.get_w3_obj()
@@ -23,16 +31,25 @@ class ContractDeployer(object):
         self.pubkey = self.acct.address
         self.compile_contract()
         self.deploy()
+        self.ipfs_hash = ""
+        if self._client is not None:
+            self.ipfs_hash = self._client.add(tools.get_contr_info_path())['Hash']
+            print("IPFS Hash is :" + self.ipfs_hash)
+            print("You can check the abi on: https://ipfs.io/ipfs/" + self.ipfs_hash)
+            print("You can check the abi on: ipfs://" + self.ipfs_hash)
+            # print(self._client.cat(self.ipfs_hash))
         if current_chain == "ropsten":
             self.assign_ens()
+        if self._client is not None:
+            self._client.close()
 
     def compile_contract(self):
         '''Compiles smart contract, creates bytecode and abi'''
 
-        #loading contract file data
+        # loading contract file data
         with open(tools.get_contract_path()) as source_file:
             source_raw = source_file.read()
-        #loading configuration data
+        # loading configuration data
         with open(tools.get_compile_data_path()) as opt_file:
             raw_opt = opt_file.read()
             opt = json.loads(raw_opt)
@@ -40,35 +57,36 @@ class ContractDeployer(object):
         opt["sources"]["BlockCertsOnchaining.sol"]["content"] = source_raw
         compiled_sol = compile_standard(opt)
 
-        #defining bytecode and abi
+        # defining bytecode and abi
         self.bytecode = compiled_sol[
             'contracts']['BlockCertsOnchaining.sol']['BlockCertsOnchaining']['evm']['bytecode']['object']
         self.abi = json.loads(compiled_sol[
-            'contracts']['BlockCertsOnchaining.sol']['BlockCertsOnchaining']['metadata'])['output']['abi']
+                                  'contracts']['BlockCertsOnchaining.sol']['BlockCertsOnchaining']['metadata'])[
+            'output']['abi']
 
     def deploy(self):
         '''Signes raw transaction and deploys it on the blockchain'''
         contract = self.w3.eth.contract(abi=self.abi, bytecode=self.bytecode)
 
-        #defining blockchain and public key of the ethereum wallet
+        # defining blockchain and public key of the ethereum wallet
         current_chain = config.config["current_chain"]
         acct_addr = self.pubkey
 
-        #building raw transaction
+        # building raw transaction
         estimated_gas = contract.constructor().estimateGas()
-        print("Estimated gas: " , estimated_gas)
+        print("Estimated gas: ", estimated_gas)
         construct_txn = contract.constructor().buildTransaction({
             'nonce': self.w3.eth.getTransactionCount(acct_addr),
             'gas': estimated_gas
         })
 
-        #signing & sending a signed transaction, saving transaction hash
+        # signing & sending a signed transaction, saving transaction hash
         signed = self.acct.sign_transaction(construct_txn)
         tx_hash = self.w3.eth.sendRawTransaction(signed.rawTransaction)
         tx_receipt = self.w3.eth.waitForTransactionReceipt(tx_hash)
-        print ("Gas used: " , tx_receipt.gasUsed)
+        print("Gas used: ", tx_receipt.gasUsed)
 
-        #saving contract data
+        # saving contract data
         with open(tools.get_contr_info_path(), "r") as f:
             raw = f.read()
             contr_info = json.loads(raw)
@@ -77,23 +95,13 @@ class ContractDeployer(object):
         contr_info["blockcertsonchaining"] = data
         with open(tools.get_contr_info_path(), "w+") as f:
             json.dump(contr_info, f)
-
-        try:
-            client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001/http')
-            res = client.add(tools.get_contr_info_path())
-            print("Uploaded " + str(res))
-            print("cat : " + str(client.cat(res['Hash'])))
-        except:
-            print("ipfs daemon is not running contract info cant be uploaded")
-        #print transaction hash
+        # print transaction hash
         print(f"deployed contr <{self.contr_address}>")
 
     def assign_ens(self):
         ens_domain = "blockcerts.eth"
         ens_resolver = ContractConnection("ropsten_ens_resolver")
 
-        # for testing purposes
-        # self.contr_address = "0x01a616157b59d75a1d62f3913c105f443232a1f6"
         self.contr_address = self.w3.toChecksumAddress(self.contr_address)
 
         ns = ENS.fromWeb3(self.w3)
@@ -101,9 +109,16 @@ class ContractDeployer(object):
 
         ens_resolver.functions.transact("setAddr", node, self.contr_address)
         ens_resolver.functions.transact("setName", node, ens_domain)
-
+        if self._client is not None:
+            try:
+                checksum_hash = self.w3.toChecksumAddress(self.ipfs_hash)
+                ens_resolver.functions.transact("setContenthash", node, checksum_hash)
+            except:
+                print(" ")
         addr = ens_resolver.functions.call("addr", node)
         name = ens_resolver.functions.call("name", node)
+        if self._client is not None:
+            content = ens_resolver.functions.call("content", node)
 
         print(f"set contr <{addr}> to name '{name}'")
 
