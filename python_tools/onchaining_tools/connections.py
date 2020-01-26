2589d19e7cc7d5a864bff3aadedadaafc72fcb65
xenia-melman <xenia.melman@gmail.com>
2020-01-26 12:57:07 +0100

    clearing comments

--- python_tools/onchaining_tools/connections.py
+++ python_tools/onchaining_tools/connections.py
@@ -7,16 +7,16 @@ from web3 import Web3, HTTPProvider
 
 
 class MakeW3(object):
-    '''
+    """
     Defines a private key of an ethereum wallet to be used for the transaction,
     node url to be used for communication with ethereum blockchain and instantiates the
     web3 connection with ethereum node
-    '''
+    """
     def __init__(self):
-        '''
+        """
         Defines public & private keys of a wallet, defines an ethereum node
         that will be used for communication with blockchain
-        '''
+        """
         current_chain = config.config["current_chain"]
         self._privkey = config.config["wallets"][current_chain]["privkey"]
         self._url = config.config["wallets"][current_chain]["url"]
@@ -27,16 +27,22 @@ class MakeW3(object):
         self.w3.eth.defaultAccount = self.pubkey
 
     def _create_w3_obj(self):
-        '''Instantiates a web3 connection with ethereum node'''
+        """
+        Instantiates a web3 connection
+        """
         return Web3(HTTPProvider(self._url))
 
     def _get_w3_wallet(self):
-        '''Connects a private key to the account that is going to be used for the transaction'''
+        """
+        Connects a private key to the account that is going to be used for the transaction
+        """
         return self.w3.eth.account.from_key(self._privkey)
 
 
 class ContractConnection(object):
-    '''Collects abi, address, contract data and instantiates a contract object'''
+    """
+    Collects abi, address, contract data and instantiates a contract object
+    """
     def __init__(self, contract_name):
         self.contract_name = contract_name
         self._w3Factory = MakeW3()
@@ -46,24 +52,32 @@ class ContractConnection(object):
         self.functions = self.ContractFunctions(self._w3Factory, self.contract_obj)
 
     def _create_contract_object(self):
-        '''Returns contract address and abi'''
+        """
+        Returns contract address and abi
+        """
         address = self._get_address()
         abi = self._get_abi()
         return self.w3.eth.contract(address=address, abi=abi)
 
     def _get_contract_info(self):
-        '''Returns transaction data from a config file'''
+        """
+        Returns transaction data from a config file
+        """
         with open(tools.get_contr_info_path()) as file:
             data = file.read()
             contract_info = json.loads(data)
         return contract_info
 
     def _get_abi(self):
-        '''Returns transaction abi'''
+        """
+        Returns transaction abi
+        """
         return self._contract_info[self.contract_name]["abi"]
 
     def _get_address(self):
-        '''Returns transaction address'''
+        """
+        Returns transaction address
+        """
         return self._contract_info[self.contract_name]["address"]
 
     class ContractFunctions(object):
@@ -77,14 +91,18 @@ class ContractConnection(object):
             self.acct_addr = self.acct.address
 
         def _get_tx_options(self, estimated_gas):
-            '''Returns raw transaction'''
+            """
+            Returns raw transaction
+            """
             return {
                 'nonce': self._w3.eth.getTransactionCount(self.acct_addr),
                 'gas': estimated_gas*2
             }
 
         def transact(self, method, *argv):
-            '''Sends a signed transaction on the blockchain and waits for a response'''
+            """
+            Sends a signed transaction on the blockchain and waits for a response
+            """
             # gas estimation
             estimated_gas = self._contract_obj.functions[method](*argv).estimateGas()
             logging.info("Estimated gas for {} : {}".format(method, estimated_gas))
